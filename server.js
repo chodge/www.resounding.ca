7d289bb4907d0e0d13587f7ad6a271eb28d63647
chodge <cliffe@resounding.ca>
2014-04-16 07:48:47 -0400

    remove node version & nodemailer

--- server.js
+++ server.js
@@ -1,38 +1,8 @@
-var nodemailer = require('nodemailer'),
-	express = require('express'),
+var express = require('express'),
 	app = express()
 	.use(express.compress())
 	.use(express.static(__dirname + '/public'))
 	.use(express.urlencoded());
-
-app.post('/work/email', function(req, res) {
-	var mailOptions = {
-    		from: 'Cliffe Hodgkinson <hodgkinson@gmail.com>',
-	    	to: 'Cliffe Hodgkinson <cliffe@resounding.ca>',
-	    	subject: 'Someone is interested in Resounding Work',
-	    	text: 'Email: ' + req.body.address,
-
-		},
-		smtpTransport = nodemailer.createTransport("SMTP", {
-	    	service: "Gmail",
-	    	auth: {
-	    		user: 'hodgkinson@gmail.com',
-	    		pass: process.env.mailpass
-	    	}
-		});
-
-	smtpTransport.sendMail(mailOptions, function(error, response){
-	    if(error) {
-	        console.log(error);
-	    } else {
-	        console.log("Message sent: " + response.message);
-	    }
-
-    	smtpTransport.close(); // shut down the connection pool, no more messages
-	});
-
-	res.end();
-});
 
 app.get('/app.manifest', function(req, res) {
 	res.set('Content-Type', 'text/cache-manifest');
